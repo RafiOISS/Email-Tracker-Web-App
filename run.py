8cf80d98cd484c60b718f279da36291f9e159592
Rafi0155 <alifabdullahsiam@gmail.com>
2025-01-09 18:59:54 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
+# create the virtual environment
+# python -m venv venv
+# source venv/bin/activate  # On Windows: venv\Scripts\activate
+# pip install -r requirements.txt
+
+# Install Required Packages
+# pip install flask flask-sqlalchemy flask-wtf flask-migrate email-validator
+
+# pip freeze > requirements.txt
+
+# Verify Flask Environment Variables
+# set FLASK_APP=run.py      # Windows
+
+# run app
+# python run.py
+#  or
+# flask run
+
+
 from app import create_app
 
 app = create_app()
